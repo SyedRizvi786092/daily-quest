88bda9547b067c0e792cf3bcae22e786eeeae33f
Mohd Mojiz Hasan <mohdmojiz.stdnt@srmu.ac.in>
2024-08-07 04:00:41 +0530

    Add Deadline DatePicker

--- app/src/main/java/com/project/dailyquest/screens/GoalsScreen.kt
+++ app/src/main/java/com/project/dailyquest/screens/GoalsScreen.kt
@@ -15,10 +15,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,15 +28,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.project.dailyquest.R
 import com.project.dailyquest.data.getDummyGoals
-import com.project.dailyquest.widgets.DisplayMainText
 import com.project.dailyquest.widgets.AppScaffold
+import com.project.dailyquest.widgets.DisplayMainText
+import com.project.dailyquest.widgets.InputTextField
 import com.project.dailyquest.widgets.NavigationBar
+import com.project.dailyquest.widgets.ShowDatePicker
 import com.project.dailyquest.widgets.ShowGoal
 
 @Preview
@@ -46,6 +53,16 @@ fun GoalsScreen(onNavigateToHome: () -> Unit = {}) {
     var addGoals by remember {
         mutableStateOf(false)
     }
+    var title by remember {
+        mutableStateOf("")
+    }
+    var description by remember {
+        mutableStateOf("")
+    }
+    var deadlinePicker by remember {
+        mutableStateOf(false)
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
     AppScaffold(title = "Goals",
         topAppBarColor = MaterialTheme.colorScheme.primaryContainer,
         homeButton = { IconButton(onClick = onNavigateToHome,
@@ -62,7 +79,8 @@ fun GoalsScreen(onNavigateToHome: () -> Unit = {}) {
             DisplayMainText(key = "Active Goals: ", value = "3", fontSize = 42.sp)
             Spacer(modifier = Modifier.height(15.dp))
             Row {
-                Button(onClick = { viewGoals = !viewGoals }) {
+                Button(onClick = { if (addGoals) addGoals = false
+                    viewGoals = !viewGoals}) {
                     Icon(painter = if (viewGoals) painterResource(id = R.drawable.hide)
                         else painterResource(id = R.drawable.view),
                         contentDescription = "View/Hide")
@@ -70,7 +88,8 @@ fun GoalsScreen(onNavigateToHome: () -> Unit = {}) {
                     Text(text = if (viewGoals) "Hide" else "View")
                 }
                 Spacer(modifier = Modifier.width(20.dp))
-                Button(onClick = { addGoals = !addGoals },
+                Button(onClick = { if (viewGoals) viewGoals = false
+                    addGoals = !addGoals},
                     enabled = !addGoals) {
                     Icon(imageVector = Icons.Default.Add,
                         contentDescription = "Add")
@@ -84,6 +103,47 @@ fun GoalsScreen(onNavigateToHome: () -> Unit = {}) {
                 LazyColumn {
                     items(items = getDummyGoals()) {
                         ShowGoal(it)
+                    }
+                }
+            }
+            AnimatedVisibility(visible = addGoals,
+                modifier = Modifier.heightIn(max = 350.dp)) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    InputTextField(text = title,
+                        onTextChange = { newText ->
+                            title = newText
+                        },
+                        label = "Title",
+                        imeAction = ImeAction.Next,
+                        onImeAction = { keyboardController?.hide() },
+                        maxLines = 2)
+                    InputTextField(modifier = Modifier.fillMaxWidth(),
+                        text = description,
+                        onTextChange = { newText ->
+                            description = newText
+                        },
+                        label = "Description",
+                        imeAction = ImeAction.Done,
+                        onImeAction = { keyboardController?.hide() },
+                        maxLines = 7,
+                        color = Color.Transparent)
+                    Spacer(modifier = Modifier.height(10.dp))
+                    TextButton(onClick = { deadlinePicker = !deadlinePicker },
+                        colors = ButtonDefaults.textButtonColors(
+                            contentColor = MaterialTheme.colorScheme.error)
+                    ) {
+                        Text(text = "Set deadline")
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Button(onClick = { addGoals = !addGoals
+                                     viewGoals = !viewGoals},
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.secondary)
+                    ) {
+                        Text(text = "Save")
+                    }
+                    if (deadlinePicker) ShowDatePicker(onDateSelected = {}) {
+                        deadlinePicker = false
                     }
                 }
             }
--- app/src/main/java/com/project/dailyquest/widgets/GoalsScreenWidgets.kt
+++ app/src/main/java/com/project/dailyquest/widgets/GoalsScreenWidgets.kt
@@ -5,29 +5,42 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.project.dailyquest.data.Goal
-import com.project.dailyquest.data.getDummyGoals
 import com.project.dailyquest.utils.getRemainingDays
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.plus
 
-@Preview(showBackground = true)
 @Composable
-fun ShowGoal(goal: Goal = getDummyGoals()[1]) {
+fun ShowGoal(goal: Goal) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -72,5 +85,59 @@ fun ShowGoal(goal: Goal = getDummyGoals()[1]) {
                     )
             }
         }
+    }
+}
+
+@Composable
+fun InputTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    onTextChange: (String) -> Unit,
+    label: String,
+    imeAction: ImeAction = ImeAction.Default,
+    onImeAction: () -> Unit = {},
+    maxLines: Int = 1,
+    color: Color = MaterialTheme.colorScheme.surfaceContainer
+) {
+    TextField(value = text, onValueChange = onTextChange,
+        modifier = modifier.padding(5.dp),
+        label = { Text(text = label) },
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction),
+        keyboardActions = KeyboardActions { onImeAction() },
+        maxLines = maxLines,
+        colors = TextFieldDefaults.colors().copy(focusedContainerColor = color,
+            unfocusedContainerColor = color)
+    )
+}
+
+@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShowDatePicker(
+    onDateSelected: (Long?) -> Unit = {},
+    onDismiss: () -> Unit = {}
+) {
+    val tomorrow = Clock.System.now().plus(48, DateTimeUnit.HOUR, TimeZone.currentSystemDefault())
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = tomorrow.toEpochMilliseconds()
+    )
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text(text = "OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text(text = "Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState,
+            title = { Text(text = "Set Deadline", modifier = Modifier.padding(16.dp)) })
     }
 }
